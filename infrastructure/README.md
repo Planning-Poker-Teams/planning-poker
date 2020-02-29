42fc37b6d4347fbc49510d3e0bf04c2f0b4a987a
Christian Stangier <christian.stangier@codecentric.de>
2020-02-29 18:11:29 +0100

    Add interfaces for lambda, remove unused dependencies

--- infrastructure/README.md
+++ infrastructure/README.md
@@ -1,14 +1,9 @@
-# Welcome to your CDK TypeScript project!
+# Getting started
 
-This is a blank project for TypeScript development with CDK.
+For runnable targets see `package.json`.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Connecting to the API
 
-## Useful commands
-
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+```bash
+npx wscat -c wss://g5ktyisvyf.execute-api.eu-central-1.amazonaws.com/dev/
+```
--- infrastructure/lib/api-stack.HelloWorld.ts
+++ infrastructure/lib/api-stack.HelloWorld.ts
@@ -1,4 +1,27 @@
-export const handler = async (event: any) => {
+interface APIGatewayLambdaInvocation {
+  requestContext: {
+    connectionId: string;
+    eventType: "CONNECT" | "MESSAGE" | "DISCONNECT";
+    connectedAt: number;
+    requestTimeEpoch: number;
+    requestId: string;
+    apiId: string;
+    stage: string;
+  };
+  body?: any; // only when eventType == MESSAGE
+  isBase64Encoded: boolean;
+}
+
+interface ProxiedLambdaResponse {
+  isBase64Encoded: boolean;
+  statusCode: number;
+  headers: object;
+  body: string; // stringified JSON
+}
+
+export const handler = async (
+  event: APIGatewayLambdaInvocation
+): Promise<ProxiedLambdaResponse> => {
   console.log("Hello world. Invocation", JSON.stringify(event));
   console.log("Body:", event.body);
 
