d7d2696270d287b84320236b0c1a2fdf401c5cfe
Christian Stangier <christian.stangier@codecentric.de>
2020-04-24 18:51:17 +0200

    Add PurgeCSS

--- web-2.0/postcss.config.js
+++ web-2.0/postcss.config.js
@@ -1,6 +1,12 @@
+const isProduction = process.env.NODE_ENV === "production";
+const purgeCss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-    "plugins": [
-      require('tailwindcss')('tailwind.js'),
-      require('autoprefixer')(),
-    ]
-  }
+  plugins: [
+    require("tailwindcss")("tailwind.js"),
+    require("autoprefixer")(),
+    isProduction
+      ? purgeCss({ content: ["./src/**/*.html", "./src/**/*.vue"] })
+      : undefined,
+  ],
+};
