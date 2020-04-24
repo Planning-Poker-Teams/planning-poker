09ad021a1c329b28e1c8608a1b93ba264091fd88
Christian Stangier <christian.stangier@codecentric.de>
2020-04-24 17:53:21 +0200

    Add a simple form validation

--- web-2.0/src/views/Lobby.vue
+++ web-2.0/src/views/Lobby.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <h1>Planning Poker</h1>
-    <p>{{ greeting }}</p>
     <form @submit.prevent>
       <label>Room name</label>
       <input v-model="roomName" />
-      <button v-on:click="joinRoom">Join Room</button>
+      <button v-on:click="joinRoom" :disabled="!formIsCompleted">
+        Join Room
+      </button>
     </form>
     <p>State: {{ joinRoomProperties }}</p>
   </div>
@@ -17,11 +18,14 @@ import Component from "vue-class-component";
 
 @Component({})
 export default class Lobby extends Vue {
-  greeting = "Hello world!";
   roomName = "";
 
   get joinRoomProperties() {
     return JSON.stringify(this.$store.state);
+  }
+
+  get formIsCompleted(): boolean {
+    return this.roomName.length > 0;
   }
 
   joinRoom() {
