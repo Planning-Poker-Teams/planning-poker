1dcf214f31815d781e4d42ea89804f97d5354064
Edward Byne <edward.byne@codecentric.de>
2020-04-24 15:53:01 +0200

    Add not working room name form

--- web-2.0/src/views/Lobby.vue
+++ web-2.0/src/views/Lobby.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>Planning Poker</h1>
     <p>{{ greeting }}</p>
+    <form @submit.prevent>
+      <label>Room name</label>
+      <input v-bind="roomName" />
+      <button v-on:click="setRoomName" />
+    </form>
   </div>
 </template>
 
@@ -9,5 +14,11 @@
 import Vue from "vue";
 export default class Lobby extends Vue {
   greeting = "Hello world!";
+  roomName = "";
+
+  setRoomName() {
+    console.log('updating room name', this.roomName)
+    this.$store.commit('roomName', this.roomName)
+  }
 }
 </script>
