633ed9605faaeea18b7ad661d9a51c212645d698
Christian Stangier <christian.stangier@codecentric.de>
2020-04-25 14:40:01 +0200

    Ignore tailwind classes for PurgeCSS

--- web-2.0/postcss.config.js
+++ web-2.0/postcss.config.js
@@ -6,7 +6,28 @@ module.exports = {
     require("tailwindcss")("tailwind.js"),
     require("autoprefixer")(),
     isProduction
-      ? purgeCss({ content: ["./src/**/*.html", "./src/**/*.vue"] })
+      ? purgeCss({
+          content: ["./src/**/*.html", "./src/**/*.vue"],
+          // The following config part is from https://purgecss.com/guides/vue.html#use-the-vue-cli-plugin
+          defaultExtractor(content) {
+            const contentWithoutStyleBlocks = content.replace(
+              /<style[^]+?<\/style>/gi,
+              ""
+            );
+            return (
+              contentWithoutStyleBlocks.match(
+                /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+              ) || []
+            );
+          },
+          whitelist: [],
+          whitelistPatterns: [
+            /-(leave|enter|appear)(|-(to|from|active))$/,
+            /^(?!(|.*?:)cursor-move).+-move$/,
+            /^router-link(|-exact)-active$/,
+            /data-v-.*/,
+          ],
+        })
       : undefined,
   ],
 };
