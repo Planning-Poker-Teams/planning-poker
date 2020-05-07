4b678e7f83e9d5a010938efb0a29f83cf70ab88f
Christian Stangier <christian.stangier@codecentric.de>
2020-05-07 18:10:19 +0200

    Set background color on body
    otherwise overscroll is shown in white

--- web/tailwind.js
+++ web/tailwind.js
@@ -1,15 +1,17 @@
 module.exports = {
-  purge: { content: ['./public/index.html', './src/**/*.html', './src/**/*.vue'] },
+  purge: {
+    content: ['./public/index.html', './src/**/*.html', './src/**/*.vue'],
+  },
   theme: {
     extend: {
       width: {
         '72': '18rem',
-        '96': '24rem'
+        '96': '24rem',
       },
       minHeight: {
         '16': '4rem',
-        '24': '6rem'
-      }
+        '24': '6rem',
+      },
     },
   },
   variants: {},
