8f8af4e6860f9c98b63ad9f2d3880b4928851c98
Christian Stangier <christian@stangier.email>
2020-06-12 11:31:53 +0200

    Improve placement of showResult button

--- web/src/components/OngoingEstimation.vue
+++ web/src/components/OngoingEstimation.vue
@@ -5,36 +5,45 @@
         ref="taskName"
         class="min-h-24 w-full max-w-lg flex justify-center items-center rounded border-4 border-gray-300 border-dashed"
       >
-        <p class="text-2xl font-medium font-sans text-center text-gray-800 p-2">{{ taskName }}</p>
+        <p class="text-2xl font-medium font-sans text-center text-gray-800 p-2">
+          {{ taskName }}
+        </p>
       </div>
+      <button
+          class="mx-2 px-6 py-2 bg-gray-300 text-gray-700 p-2 border-2 hover:border-gray-400 border-gray-300 rounded"
+          type="submit"
+          v-if="votingIsComplete"
+          @click="requestResult"
+        >
+          Show result
+        </button>
     </div>
 
     <div class="grid grid-cols-4 col-gap-2 row-gap-2 mb-4" v-if="!isSpectator">
       <div
         class="flex flex-col justify-center w-16 lg:w-20 h-24 lg:h-32 rounded-lg shadow cursor-pointer select-none relative"
+        :class="value == selectedEstimation ? 'bg-red-400 opacity-90' : 'bg-blue-400'"
         v-for="value in possibleEstimationValues"
         :ref="`card-${value}`"
         :key="value"
-        :class="value == selectedEstimation ? 'bg-red-400' : 'bg-blue-400'"
         @click="sendEstimation(value)"
       >
-        <p class="absolute top-0 left-0 text-sm text-white px-1 font-mono">{{ value }}</p>
-        <p class="text-white font-medium text-2xl text-center font-mono">{{ value }}</p>
+        <p class="absolute top-0 left-0 text-sm text-white px-1 font-mono">
+          {{ value }}
+        </p>
+        <p class="text-white font-medium text-2xl text-center font-mono">
+          {{ value }}
+        </p>
         <p
           class="absolute bottom-0 right-0 transform rotate-180 text-sm text-white px-1 font-mono"
-        >{{ value }}</p>
+        >
+          {{ value }}
+        </p>
       </div>
     </div>
     <div class="row-span-2 flex items-center justify-center" v-if="isSpectator">
       <p class="font-medium text-4xl text-gray-500">Participants are voting</p>
     </div>
-
-    <button
-      class="mb-4 px-6 py-2 bg-gray-300 text-gray-700 p-2 border-2 hover:border-gray-400 border-gray-300 rounded"
-      type="submit"
-      v-if="votingIsComplete"
-      @click="requestResult"
-    >Show result</button>
   </section>
 </template>
 
