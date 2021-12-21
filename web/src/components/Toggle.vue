1c30e128a62322b96bff46f4171de1ad6d9fe299
Johannes Barop <johannes.barop@codecentric.de>
2021-12-21 19:02:01 +0100

    fix eslint warning

--- web/src/components/Toggle.vue
+++ web/src/components/Toggle.vue
@@ -1,13 +1,7 @@
 <template>
   <label :for="id" class="flex items-center cursor-pointer">
     <div class="relative">
-      <input
-        :id="id"
-        class="hidden"
-        type="checkbox"
-        :checked="modelValue"
-        @change="onChange"
-      />
+      <input :id="id" class="hidden" type="checkbox" :checked="modelValue" @change="onChange" />
       <div class="toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
       <div class="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"></div>
     </div>
