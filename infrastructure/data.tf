0ef1734e5c9245ecd3abe9be26d26deae45879d4
Johannes Barop <johannes.barop@codecentric.de>
2022-11-15 07:44:43 +0100

    Use planningpoker.cc in deployment

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,3 +1,16 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
+
+data "aws_route53_zone" "main" {
+  name = "planningpoker.cc"
+}
+
+data "aws_acm_certificate" "backend" {
+  domain = "*.${data.aws_route53_zone.main.name}"
+}
+
+data "aws_acm_certificate" "frontend" {
+  domain   = "*.${data.aws_route53_zone.main.name}"
+  provider = aws.virginia
+}
