fd329543603411e9c4848d43c9b2d9a1aeeb03fb
Yannic Lades <yannic.lades@codecentric.de>
2023-09-19 15:02:46 +0200

    added basic length limitation for room name and username

--- packages/frontend/src/views/Lobby.vue
+++ packages/frontend/src/views/Lobby.vue
@@ -15,12 +15,14 @@
       <div class="flex flex-col">
         <input
           v-model="roomName"
+          maxlength="15"
           class="mb-2 py-2 px-3 text-lg font-semi bg-white appearance-none border-2 rounded text-grey-darker focus:outline-none focus:border-green-300"
           placeholder="Room name"
           @input="onRoomName"
         />
         <input
           v-model="userName"
+          maxlength="15"
           class="mb-2 py-2 px-3 text-lg font-semi bg-white appearance-none border-2 rounded text-grey-darker focus:outline-none focus:border-green-300"
           placeholder="Your name"
           @input="onUserName"
@@ -49,13 +51,16 @@
           </button>
           <p class="my-2 mr-2 justify-center text-sm text-gray-700">
             By clicking "Join room" you agree to our
-            <a class="underline text-gray-700" href="/privacy">Privacy Policy</a>.
+            <a class="underline text-gray-700" href="/privacy">Privacy Policy</a
+            >.
           </p>
         </div>
 
         <div class="my-4x flex flex-col items-center">
           <div class="my-4 w-full rounded border border-gray-200" />
-          <a class="underline text-gray-700" href="/legal-notice">Legal notice</a>
+          <a class="underline text-gray-700" href="/legal-notice"
+            >Legal notice</a
+          >
         </div>
       </div>
     </form>
@@ -63,25 +68,25 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { Store, useStore } from 'vuex';
-import Toggle from '../components/Toggle.vue';
-import { useStorage } from '../hooks/useStorage';
-import { MutationsType } from '../store/mutations';
-import { State } from '../store/types';
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { Store, useStore } from "vuex";
+import Toggle from "../components/Toggle.vue";
+import { useStorage } from "../hooks/useStorage";
+import { MutationsType } from "../store/mutations";
+import { State } from "../store/types";
 
 const router = useRouter();
 const route = useRoute();
 const store: Store<State> = useStore();
 
-const storageUserName = useStorage('userName');
+const storageUserName = useStorage("userName");
 const userName = ref(storageUserName.value);
 const onUserName = (event: Event) => {
   storageUserName.value = event.target?.value;
 };
 
-const storageRoomName = useStorage('roomName');
+const storageRoomName = useStorage("roomName");
 const roomName = ref(storageRoomName.value);
 const onRoomName = (event: Event) => {
   storageRoomName.value = event.target?.value;
@@ -90,7 +95,7 @@ const onRoomName = (event: Event) => {
 const isSpectator = ref(false);
 const showCats = ref(true);
 const roomNameQuery = route.query.room as string;
-if (typeof roomNameQuery === 'string' && roomNameQuery.length > 0) {
+if (typeof roomNameQuery === "string" && roomNameQuery.length > 0) {
   roomName.value = roomNameQuery;
 }
 
@@ -101,11 +106,11 @@ const joinRoom = () => {
     isSpectator: isSpectator,
     showCats: showCats,
   });
-  router.push({ name: 'room', params: { roomName: roomName.value } });
+  router.push({ name: "room", params: { roomName: roomName.value } });
 };
 
 const formIsCompleted = computed((): boolean => {
-  return typeof roomName.value !== 'undefined' && userName.value.length > 0;
+  return typeof roomName.value !== "undefined" && userName.value.length > 0;
 });
 
 defineExpose({
