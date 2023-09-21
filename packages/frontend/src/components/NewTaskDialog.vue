1bb425b4c464301db467c108166fdc3e8ae39a3e
Yannic Lades <yannic.lades@codecentric.de>
2023-09-21 10:29:22 +0200

    optimisations to newtaskdialog

--- packages/frontend/src/components/NewTaskDialog.vue
+++ packages/frontend/src/components/NewTaskDialog.vue
@@ -5,33 +5,28 @@
   >
     <div class="mx-auto p-5 border w-2/4 shadow-lg rounded-md bg-white">
       <div class="mt-3 text-center">
-        <template v-if="pendingParticipants.length">
-          <p class="text-lg leading-6 font-medium text-gray-900">
-            What do you want to vote on?
-          </p>
+        <p class="text-lg leading-6 font-medium text-gray-900">
+          What do you want to vote on?
+        </p>
 
-          <input
-            v-model="newTaskName"
-            class="p-2 mb-4 w-full lg:w-1/2 text-center text-lg font-semi bg-white appearance-none border-4 rounded text-grey-darker focus:outline-none focus:border-green-300"
-            placeholder="Task name"
-          />
-        </template>
-
-        <template v-else>
-          <p class="text-lg mb-2 leading-6 font-medium text-gray-900">
-            Everyone has voted, proceed to view the results by clicking "OK"!
-          </p>
-        </template>
+        <input
+          v-model="newTaskName"
+          maxlength="20"
+          class="p-2 mb-4 w-full lg:w-1/2 text-center text-lg font-semi bg-white appearance-none border-4 rounded text-grey-darker focus:outline-none focus:border-green-300"
+          placeholder="Task name"
+        />
 
         <div class="w-5/6 mx-auto items-center px-4 py-3">
           <button
-            class="ml-1 px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+            class="mr-1 px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-green-300"
             @click="cancel"
           >
             CANCEL
           </button>
           <button
-            class="mr-1 px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+            class="ml-1 px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-5/12 shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+            :class="{ 'bg-gray-300 hover:bg-gray-300': newTaskName.length < 1 }"
+            :disabled="newTaskName.length < 1"
             @click="confirm"
           >
             CREATE
@@ -43,19 +38,15 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref, toRef, computed } from "vue";
+import { ref } from "vue";
 import { Store, useStore } from "vuex";
 import { ActionType } from "../store/actions";
-import { Participant, State } from "../store/types";
+import { State } from "../store/types";
 
 const newTaskName = ref("");
 
 const emits = defineEmits(["on_confirm", "on_cancel"]);
 const store: Store<State> = useStore();
-const pendingParticipants: Ref<Participant[]> = toRef(
-  store.getters,
-  "pendingParticipants"
-);
 
 const confirm = () => {
   startEstimation(newTaskName.value);
@@ -65,12 +56,4 @@ const startEstimation = async (taskName: string) => {
   store.dispatch(ActionType.REQUEST_START_ESTIMATION, taskName);
 };
 const cancel = () => emits("on_cancel");
-
-const userName = computed(() => {
-  return store.state.room?.userName;
-});
-
-const removeUser = (userName: string) => {
-  store.dispatch(ActionType.REMOVE_USER, userName);
-};
 </script>
